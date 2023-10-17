e2e935fca64be5f05626b628239c970b4ae0115e
linu-paulose <113226778+linu-paulose@users.noreply.github.com>
2023-10-17 11:12:44 +0530

    Add files via upload

---
+++ arr2string.js
@@ -0,0 +1,10 @@
+let fruits =['banana','apple','orange','kiwi']
+fruit_len= fruits.length
+let new_string =''
+for(let i=0; i < fruit_len; i++){
+    new_string += fruits[i];
+    new_string += " ";
+    }
+
+
+console.log( new_string )
